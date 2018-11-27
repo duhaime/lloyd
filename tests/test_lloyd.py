a61d0b3249a3b1621d399d561a94bf413e40e9ed
duhaime <douglas.duhaime@gmail.com>
2018-11-27 17:19:59 -0500

    test constrained and unconstrained models

--- tests/test_lloyd.py
+++ tests/test_lloyd.py
@@ -7,34 +7,47 @@ import numpy as np
 @pytest.fixture()
 def arr():
   np.random.seed(141)
-  return np.random.rand(100,2)
+  arr = np.random.rand(100,2)
+  # create duplicate inputs
+  arr[1][0] = arr[0][0]
+  arr[1][1] = arr[0][1]
+  return arr
 
 @pytest.fixture()
-def field(arr):
+def constrained(arr):
   field = Field(arr)
   field.relax()
   return field
 
-def test_result_points_same_shape(arr, field):
+@pytest.fixture()
+def unconstrained(arr):
+  field = Field(arr, constrained=False)
+  field.relax()
+  return field
+
+def test_result_points_same_shape(arr, constrained, unconstrained):
   '''
   Ensure the points returned from the transformation
   have the same shape as the input points
   '''
-  assert field.get_points().shape == arr.shape
+  for field in [constrained, unconstrained]:
+    assert field.get_points().shape == arr.shape
 
-def test_points_in_bb(field):
+
+def test_points_in_bb(constrained):
   '''
   Ensure all input points are in the bounding box
   '''
-  for x, y in field.get_points():
-    assert x >= field.bb[0]
-    assert x <= field.bb[1]
-    assert y >= field.bb[2]
-    assert y <= field.bb[3]
+  for x, y in constrained.get_points():
+    assert x >= constrained.bb[0]
+    assert x <= constrained.bb[1]
+    assert y >= constrained.bb[2]
+    assert y <= constrained.bb[3]
 
-def test_no_empty_regions(field):
+def test_no_empty_regions(constrained, unconstrained):
   '''
   Ensure there are no empty regions in voronoi.regions.
   '''
-  for i in field.voronoi.regions:
-    assert len(i) > 0
+  for field in [constrained, unconstrained]:
+    for i in field.voronoi.regions:
+      assert len(i) > 0
