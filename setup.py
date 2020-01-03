84fb348d3beeead52dd5612e2bbd08cf03f45a9a
duhaime <douglas.duhaime@gmail.com>
2020-01-03 15:58:34 -0500

    silence prints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
   name='lloyd',
-  version='0.0.9',
+  version='0.0.10',
   packages=['lloyd'],
   keywords = ['data-visualization', 'geometry', 'voronoi', 'lloyd-iteration'],
   description='Lloyd iteration for distributing points in two-dimensional space.',
